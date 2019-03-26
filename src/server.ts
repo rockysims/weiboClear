a28ff7501ba357187f450351e165b5ba9db096a5
Rocky Sims <rocky.sims@gmail.com>
2019-03-26 00:31:06 -0700

    First attempt at communicating with weibo api.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,33 @@
 import express from 'express';
+const SinaWeibo = require('node-sina-weibo');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
 	res.send('working');
+});
+
+app.get('/go', (req, res) => {
+	const weibo = new SinaWeibo(process.env.WEIBO_CLIENT_ID, process.env.WEIBO_CLIENT_SECRET);
+
+	const url = weibo.getAuthorizeUrl({
+		redirect_uri:'https://weibo-clear.herokuapp.com/weiboAuthorizeUrlCallback',
+		response_type:'code'
+	});
+
+	res.write('url: ');
+	res.write(url);
+	res.write(' end.');
+	res.end();
+});
+
+app.get('/weiboAuthorizeUrlCallback', (req, res) => {
+	res.send('/weiboAuthorizeUrlCallback working');
+});
+
+app.get('/sinaweibo/callback', (req, res) => {
+	res.send('/sinaweibo/callback working');
 });
 
 app.listen(port);
