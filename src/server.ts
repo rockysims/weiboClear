58082710054774901a0e66baf5f402e6cfcbbd57
Rocky Sims <rocky.sims@gmail.com>
2019-03-27 02:02:48 -0700

    Removed some console.log()s and change weibo api url to all lowercase.

--- src/server.ts
+++ src/server.ts
@@ -20,17 +20,15 @@ app.get('/login', (req, res) => {
 });
 
 app.get('/sinaweibo/callback', (req, res) => {
-	console.log('req.query.code: ', req.query.code);
 	weibo.getAccessToken({
 			code: req.query.code,
 			grant_type: 'authorization_code',
 			redirect_uri: 'https://weibo-clear.herokuapp.com/sinaweibo/callback'
 		}, function (err: any, result: any, accessToken: string) {
-			console.log('accessToken: ', accessToken);
 			if (err) res.send(err);
 			else {
-				weibo.GET('Friendships/followers', {}, function(result: any) {
-					console.log('Friendships/followers result: ', result);
+				weibo.GET('friendships/followers', {}, function(result: any) {
+					console.log('friendships/followers result: ', result);
 					res.send(result);
 				});
 			}
