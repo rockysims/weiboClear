d9fb0d2246ebf363f629df9327d1db818544dc24
Rocky Sims <rocky.sims@gmail.com>
2019-03-27 01:46:15 -0700

    Working on making an actual weibo api call.

--- src/server.ts
+++ src/server.ts
@@ -4,26 +4,40 @@ const SinaWeibo = require('node-sina-weibo');
 const app = express();
 const port = process.env.PORT || 8080;
 
+const weibo = new SinaWeibo(process.env.WEIBO_CLIENT_ID, process.env.WEIBO_CLIENT_SECRET);
+
 app.get('/', (req, res) => {
 	res.send('working');
 });
 
-app.get('/go', (req, res) => {
-	const weibo = new SinaWeibo(process.env.WEIBO_CLIENT_ID, process.env.WEIBO_CLIENT_SECRET);
-
-	const url = weibo.getAuthorizeUrl({
+app.get('/login', (req, res) => {
+	const weiboLoginUrl = weibo.getAuthorizeUrl({
 		redirect_uri:'https://weibo-clear.herokuapp.com/sinaweibo/callback',
 		response_type:'code'
 	});
 
-	res.write('url: ');
-	res.write(url);
-	res.write(' end.');
-	res.end();
+	res.redirect(weiboLoginUrl);
 });
 
 app.get('/sinaweibo/callback', (req, res) => {
-	res.send('/sinaweibo/callback working');
+	weibo.getAccessToken({
+			code: req.params.code,
+			grant_type: 'authorization_code',
+			redirect_uri: 'https://weibo-clear.herokuapp.com/sinaweibo/callback2'
+		}, function (err: any, result: any, accessToken: string) {
+			if (err) res.send(err);
+			else {
+				weibo.GET('Friendships/followers', {}, function(result: any) {
+					console.log('Friendships/followers result: ', result);
+					res.send(result);
+				});
+			}
+		}
+	);
+});
+
+app.get('/sinaweibo/callback2', (req, res) => {
+	res.send('/sinaweibo/callback2 working');
 });
 
 app.listen(port);
